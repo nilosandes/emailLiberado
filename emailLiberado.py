5d0bf0914fb21c9eb9dd7295b2a03306838d905c
Nilo Sandes <71954555+nilosandes@users.noreply.github.com>
2024-10-04 11:18:08 -0300

    V2 do codigo

--- emailLiberado.py
+++ emailLiberado.py
@@ -6,6 +6,18 @@ import pytz
 # Alterna para a última janela aberta (deve ser o email)
 pyg.hotkey('alt', 'tab')
 
+pyg.hotkey('ctrl', 'f')
+pyg.write('De:')
+pyg.press('enter')
+pyg.press('esc')
+pyg.press('right')
+pyg.press('right')
+pyg.hotkey('ctrl', 'shiftleft', 'shiftright', 'right')
+pyg.PAUSE = 0.4
+pyg.hotkey('ctrl', 'c')
+pyg.PAUSE = 0.4
+pyg.hotkey('ctrl', 'home')
+
 # Obtém a hora atual e converter para INT
 timezone = pytz.timezone('America/Sao_Paulo')
 now = datetime.now(timezone)
@@ -15,11 +27,13 @@ print (type(horaAtual))
 # Confere se é manhã ou tarde
 saudacao = ""
 if horaAtual<12:
-  saudacao = "Bom dia!"
+  saudacao = "Bom dia, "
 else:
-  saudacao = "Boa tarde!"
+  saudacao = "Boa tarde, "
 
 #Comandos para responder o e-mail
-pyg.write(f'{saudacao}\n\nEmail liberado.', interval=0.02)
+pyg.write(f'{saudacao}')
+pyg.hotkey('ctrl', 'v')
+pyg.write('!\n\nEmail liberado.', interval=0.02)
 pyg.hotkey('ctrl', 'enter')
 pyg.press('enter')
